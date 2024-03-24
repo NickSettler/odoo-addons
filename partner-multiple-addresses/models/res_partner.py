cda2f57037fe9e0ca19de4e98ca9457708fae5d7
NickSettler <moiseevnikita14@gmail.com>
2024-03-24 21:41:39 +0100

    feat(partner): add supporting exporting multiple addresses to vCard

--- partner-multiple-addresses/models/res_partner.py
+++ partner-multiple-addresses/models/res_partner.py
@@ -1,4 +1,14 @@
+import logging
 from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+try:
+    import vobject
+except ImportError:
+    _logger.warning(
+        "`vobject` Python module not found, vcard file generation disabled. Consider installing this module if you want to generate vcard files")
+    vobject = None
 
 
 class Partner(models.Model):
@@ -26,3 +36,25 @@ class Partner(models.Model):
 
             name = f"{self.commercial_company_name or self.sudo().parent_id.name}, {name}"
         return name.strip()
+
+    def _build_vcard(self):
+        vcard = super(Partner, self)._build_vcard()
+
+        allowed_vcard_adr_types = ('home', 'school', 'work')
+
+        if vcard.adr and self.type in allowed_vcard_adr_types:
+            vcard.adr.type_param = self.type
+
+        for child in self.child_ids:
+            if child.type not in allowed_vcard_adr_types:
+                continue
+            adr = vcard.add('ADR')
+            adr.value = vobject.vcard.Address(street=child.street or '', city=child.city or '', code=child.zip or '')
+            if child.state_id:
+                adr.value.region = child.state_id.name
+            if child.country_id:
+                adr.value.country = child.country_id.name
+
+            adr.type_param = child.type
+
+        return vcard
